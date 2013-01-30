4807e4bd8ea63bc48cd40921a6d22fbfa017c69b
Brendan Tobolaski <btobolaski@aero.und.edu>
2013-01-30 10:46:31 -0600

    Outlined how I will write the plugin

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: fuzzy-hipster
+ * Plugin URI: https://github.com/btobolaski/fuzzy-hipster
+ * Description: A plugin in to do pull quotes in the way that a List Apart has suggested doing them
+ * Revision: 0.1
+ * Author: Brendan Tobolaski
+ * Author URI: http://tobolaski.com
+ * License:
+ */
+
+// List the shortcodes that this plugin uses
+// The function that actually makes the output
